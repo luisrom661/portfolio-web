4183321a540abc8a2d4ad6654185450b3d724416
luisrom661 <luisrom@unicauca.edu.co>
2023-12-03 21:31:12 -0500

    Add Docker and Jenkins images, integrate Tailwind CSS

---
+++ src/assets/data.js
@@ -0,0 +1,100 @@
+export const projects = [
+  {
+    title: "Nest Ecommerce",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .",
+    image: "jenkins-480",
+    color: "#f3c201",
+    urlProject: "https://github.com/luisrom661/nest-ecommerce",
+    liveDemo: "",
+  },
+  {
+    title: "Nest Ecommerce",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .",
+    image: "jenkins-480",
+    color: "#f3c201",
+    urlProject: "https://github.com/luisrom661/nest-ecommerce",
+    liveDemo: "",
+  },
+  {
+    title: "Nest Ecommerce",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .",
+    image: "jenkins-480",
+    color: "#f3c201",
+    urlProject: "https://github.com/luisrom661/nest-ecommerce",
+    liveDemo: "",
+  },
+  {
+    title: "Nest Ecommerce",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .",
+    image: "jenkins-480",
+    color: "#f3c201",
+    urlProject: "https://github.com/luisrom661/nest-ecommerce",
+    liveDemo: "",
+  },
+  {
+    title: "Nest Ecommerce",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .",
+    image: "jenkins-480",
+    color: "#f3c201",
+    urlProject: "https://github.com/luisrom661/nest-ecommerce",
+    liveDemo: "",
+  },
+  {
+    title: "Node Ecommerce",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. .",
+    image: "docker-512",
+    color: "#1629ab",
+    urlProject: "https://github.com/luisrom661/node-restserver",
+    liveDemo: "",
+  },
+  {
+    title: "Tadhack Backend",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "terraform-480",
+    color: "#045fb7",
+    urlProject: "https://github.com/luisrom661/tadhack-backend",
+    liveDemo: "https://tadhack-backend.zeabur.app/api/users",
+  },
+  {
+    title: "Products API",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "deno-480",
+    color: "#146866",
+    urlProject: "https://github.com/luisrom661/spring-java-microservices",
+    liveDemo: "",
+  },
+  {
+    title: "Products API",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "deno-480",
+    color: "#146866",
+    urlProject: "https://github.com/luisrom661/spring-java-microservices",
+    liveDemo: "",
+  },
+  {
+    title: "Products API",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat voluptatem ad, exercitationem in beatae voluptate fugit hic error, et quod maxime repellat iusto! Iusto nisi soluta hic minima! Temporibus sequi incidunt vitae dolorum error illo ducimus saepe blanditiis pariatur. Optio amet cum excepturi dignissimos aut tenetur repudiandae ad dolorem, distinctio minima inventore, neque adipisci, facilis autem quis quod nemo. Delectus sint deserunt repellat ea odio cupiditate repellendus eius saepe ipsa dicta vel nulla ullam sit deleniti accusantium quas dolore, veritatis dolores totam vitae voluptatem placeat fugiat similique alias? Possimus numquam aut illo velit repr.",
+    image: "deno-480",
+    color: "#146866",
+    urlProject: "https://github.com/luisrom661/spring-java-microservices",
+    liveDemo: "",
+  },
+];
+
+export const buttons = [
+  {
+    github: "bx bxl-github",
+    demo: "bx bx-desktop",
+    back: "bx bxs-left-arrow-circle",
+  },
+];
